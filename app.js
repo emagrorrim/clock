48d421ea89bbdfb1411bb35019df39ce3ae3b0ca
Xin Guo <emagrorrimxguo@gmail.com>
2017-09-10 16:26:12 +0800

    Update configuration

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
 app.use(bodyParser.json({limit: '50mb'}));       
 app.use(bodyParser.urlencoded({limit: "50mb", extended: true}));
 
-app.use(express.static('./public/dist'));
+app.use(express.static('./public'));
 
 app.get('/', (req, res, next) => {
   res.sendFile(path.resolve(__dirname, './public/dist/index.html'))
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "./public/dist"),
     filename: "[name].js",
-    publicPath: "."
+    publicPath: "./dist"
   },
   module: {
     rules: [
