9207f923b83267c0abd2e55e91dfcf17d6e95bab
Xin Guo <emagrorrimxguo@gmail.com>
2017-09-10 16:45:11 +0800

    Add Clock container

--- src/app/App.js
+++ src/app/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <p>Hello World!</p>
+        <Clock />
       </div>
     );
   }
---
+++ src/clock/component/Clock.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+import TimingLabel from './TimingLabel';
+
+export default class Clock extends Component {
+  render() {
+    return (
+      <div className="darkBackground">
+      </div>
+    )
+  }
+}
